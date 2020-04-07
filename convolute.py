318afa3d0b036cb5780e9ff58197eecb9a5fe035
PopeJuliusII <edgar@Pactolus.local>
2020-04-07 14:21:36 -0400

    convolute docstring addition.

--- convolute.py
+++ convolute.py
@@ -1,2 +1,11 @@
-def triple_convolute(A, B, C):
+def convolute(A, B, C):
+    """
+    This function is more the convolution of three values.
+    It raises the first argument to the power of the second,
+    and then divided by one less than the third. 
+    :param A: This is the value returned by afunc.
+    :param B: This is the value returned by bfunc.
+    :param C: This is the value returned by cfunc.
+    :return: The returned value is a floating point.
+    """
     return (A ** B) / (C - 1)
