65d80c77d09518deac1d0b658a67742fb0aafe41
tommydamiano <59709543+tommydamiano@users.noreply.github.com>
2020-04-07 14:24:26 -0400

    Updates to flask server

--- server.py
+++ server.py
@@ -4,11 +4,9 @@ from flask_cors import CORS
 app = Flask(__name__)
 CORS(app)
 
-@app.route('/api/webpage', methods=['GET'])
+@app.route('/', methods=['GET'])
 def webpage():
-    #insert HTML page in quotes to render it out 
-    return render_template('')
-
+    return render_template('home.html')
 
 if __name__ == '__main__':
     app.run(debug=True)
